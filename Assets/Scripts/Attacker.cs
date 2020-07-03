1f85df3920528629ae589ed41fbd6c9a846ecb76
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-07-02 17:18:39 -0700

    creating animations for lizard and setting up play area

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+    
+    [SerializeField] [Range(0, 5f)] float walkSpeed = 1.5f;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Vector2.anyDirection!
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+    [SerializeField] int loadingDelay = 3;
+
+    int currentSceneIdx;
+    // Start is called before the first frame update
+    void Start() {
+        currentSceneIdx = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIdx == 0) {
+            StartCoroutine(DelayedNavigation());
+        }
+    }
+
+    IEnumerator DelayedNavigation() {
+        yield return new WaitForSeconds(loadingDelay);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene() {
+        currentSceneIdx++;
+        SceneManager.LoadScene(currentSceneIdx);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
