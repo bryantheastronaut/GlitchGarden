f44f8ab61f7f1a32cec65dedfdb11dfb54b42f7a
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-07-05 14:21:48 -0700

    adding projectile

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    [SerializeField] Projectile projectilePrefab;
+    [SerializeField] GameObject gun;
+
+    public void Fire() {
+        Instantiate(projectilePrefab, gun.transform.position, Quaternion.identity);
+    }
+}
