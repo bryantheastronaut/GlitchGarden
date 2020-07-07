71d8de0d242bf6cba4ffb4f22822df408c8f9c6f
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-07-06 17:44:48 -0700

    spawn on clicked location

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+    [SerializeField] GameObject defenderPrefab;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    private void OnMouseDown() {
+        SpawnDefender(GetSquareClicked());
+
+    }
+
+    private Vector2 GetSquareClicked() {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    private void SpawnDefender(Vector2 spawnPoint) {
+        Instantiate(defenderPrefab, spawnPoint, Quaternion.identity);
+    }
+}
