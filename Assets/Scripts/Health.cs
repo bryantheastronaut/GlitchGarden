75e5eae171fd68e0517a0cf4efd0104ee172387d
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-07-06 17:14:27 -0700

    adding projectile damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVfx;
+
+    public void InflictDamage(float amount) {
+        health -= amount;
+        ReactToDamage();
+    }
+
+    private void ReactToDamage() {
+        if (health <= 0) {
+            TriggerDeathVFX();
+            Destroy(gameObject);
+        }
+    }
+
+    private void TriggerDeathVFX() {
+        if (!deathVfx) return;
+        var effect = Instantiate(deathVfx, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
     [SerializeField] float speed = 7.5f;
+    [SerializeField] float damage = 50f;
 
     void Update() {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Attacker")) {
+            var health = collision.GetComponent<Health>();
+            var attacker = collision.GetComponent<Attacker>();
+            if (health && attacker) {
+                health.InflictDamage(damage);
+                Destroy(gameObject);
+            }
+
+        }
     }
 }
